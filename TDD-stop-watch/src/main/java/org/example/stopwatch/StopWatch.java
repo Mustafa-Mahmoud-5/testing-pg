f63e4dbd9053940d994bd664b3a401b2d13476f2
Mustafa-Mahmoud-5 <mustafamahmoud4004@gmail.com>
2025-08-16 12:54:39 +0300

    tdd-pass(stopWatch): created a new stopwatch class with minimal getters

---
+++ TDD-stop-watch/src/main/java/org/example/stopwatch/StopWatch.java
@@ -0,0 +1,20 @@
+package org.example.stopwatch;
+
+public class StopWatch {
+    private int minutes;
+    private int hours;
+    private int days;
+
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+}
