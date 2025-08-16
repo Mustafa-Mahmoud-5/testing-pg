10cf8e6a94c07b6bc6656a5195823bd234964c78
Mustafa-Mahmoud-5 <mustafamahmoud4004@gmail.com>
2025-08-16 13:07:22 +0300

    tdd-pass(addMinutes): implemented addMinutes method that throws if a negative param is passed

--- TDD-stop-watch/src/main/java/org/example/stopwatch/StopWatch.java
+++ TDD-stop-watch/src/main/java/org/example/stopwatch/StopWatch.java
@@ -1,5 +1,7 @@
 package org.example.stopwatch;
 
+import java.security.InvalidParameterException;
+
 public class StopWatch {
     private int minutes;
     private int hours;
@@ -10,6 +12,12 @@ public class StopWatch {
         this.minutes = 0;
         this.days = 0;
         this.hours = 0;
+    }
+
+    public void addMinutes(int minutes) {
+        if(minutes < 0) throw new InvalidParameterException("minutes should be > 0");
+
+        this.minutes+= minutes;
     }
 
 
